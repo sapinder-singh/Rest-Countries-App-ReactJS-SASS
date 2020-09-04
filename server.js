8bca987d8290c9e2fdbfa569c33c5d07aefae4bd
Sapinder <sappiethird@gmail.com>
2020-09-04 12:54:47 +0530

    fixed server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/build'));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname), 'build/index.html');
+	res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(port);
